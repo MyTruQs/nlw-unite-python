5233d2d31535c784752240787782ded67c8c392c
root <root@Raul>
2024-04-02 20:50:18 -0300

    improve: Addin try in events repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -15,10 +15,11 @@ def test_insert_event():
     events_repository = EventsRepository()
     response = events_repository.insert_event(event)
     print(response)
-    
+
+#@pytest.mark.skip(reason="Nao necessita")    
 def test_get_event_by_id():
-    event_id = "meu-uuid-e-nois2"
+    event_id = "meu-uuid-e-nois22"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
-    print(response.title)
+    #print(response.title)
