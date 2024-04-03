37eef0517c55ce59a19decb51d64c77802fbd530
root <root@Raul>
2024-04-03 07:32:29 -0300

    feat: Implementing http_types

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import Httpre
 
 event_route_bp = Blueprint("event_route", __name__)
 
 @event_route_bp.route('/events', methods=['POST'])
 def create_event():
-    return jsonify({"ola": "mundo"}),200
+    
+    return jsonify({"ola": "mundo"})
